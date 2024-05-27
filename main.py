98c99920461ec9c583abcdc4669990d25c7fcf91
OptiviewTech <optiviewcompany@gmail.com>
2024-05-27 08:54:17 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+a = int(input("Primeiro numero"))
+b = int(input("Segundo numero:"))
+c = input("Operação:")
+
+if c = 'adição':
+  print(a+b)
+if c = 'subtração':
+  print(a-b)
+if c = 'multiplicação':
+  print(a*b)
+if c = 'diwvisão':
+  print(a/b)
